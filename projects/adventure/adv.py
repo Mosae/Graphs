98937c3022bd99d0f91a3d8b45d4d9e8cb4b8fb6
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-07-17 13:38:29 -0500

    Added notes to explain steps

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -24,6 +24,23 @@ world.load_graph(room_graph)
 world.print_rooms()
 
 player = Player(world.starting_room)
+########################################
+#BASIC IDEA 
+#We record the room we in to visited dict
+#Find all the exits for that room
+#Move in 1 direction - record that direction to traversal_path 
+#and pop off direction connected to the room
+#Find opposite direction - add that to opposite/reverse path. 
+#This will help with going backwards when possible.
+#also remove the opposite direction from paths not traveled yet
+#Find exits for new room - store them in visited
+#Move in a different direction, add that to traversap_path - pop it off possible direction
+# Move till you get to the end and cannot move anymore
+# Once there are no more exits - go backwards the same way and remove that from backwrds path
+# add that to the traversal_path
+#Check that room for more drections and repeat the process(traversal)
+#This will happen till the visited rooms = len(room_graph)
+#######################################
 
 # Fill this out with directions to walk
 #breakpoint()
@@ -33,14 +50,15 @@ visited = {}
 #store the paths -main path
 main_path = []
 #append current room to visited 
-# at the given room - find the exit
-visited[player.current_room.id] = player.current_room.get_exits()
-#print(visited)
 #how to move in oppoiste direction
 oppoiste_direction = {'n': 's', 'e': 'w', 's': 'n', 'w': 'e'}
 
+# at the given room(first) to dictionary - find the exit
+visited[player.current_room.id] = player.current_room.get_exits()
+#print(visited)
+
 # Begin BFS 
-#if the length of visited rooms is less than graph rooms. we need to keep going
+#While the length of visited rooms is less than graph rooms. we need to keep going
 while len(visited) < len(room_graph) -1:
     #find out if the visited room is in the visited dict - if we have been there before
     if player.current_room.id not in visited:
@@ -52,7 +70,7 @@ while len(visited) < len(room_graph) -1:
         visited[player.current_room.id].remove(direction_before)
         #print(direction_before)
     #change direction/traversal to look at all rooms
-    while len(visited[player.current_room.id]) == 0:
+    if len(visited[player.current_room.id])== 0:
         #get rid of the previous exit
         direction_before = main_path.pop()
         #add those exits to traversal_path
@@ -60,14 +78,20 @@ while len(visited) < len(room_graph) -1:
         #print(traversal_path)
         #move player to previous room and check if we visited before
         player.travel(direction_before)
+
+    #else if there are directions not expoited
+    # add that direction to the traversal_path
+    # go eslore new room     
     #find the current exit to the room and last room in list -> go to that room
-    move_to_next_room = visited[player.current_room.id].pop(0)
-    #add that move to the traversal_path
-    traversal_path.append(move_to_next_room) 
-    #also add it to the main path
-    main_path.append(oppoiste_direction[move_to_next_room])
-    #go backwards through rooms
-    player.travel(move_to_next_room)
+    else:
+        move_to_next_room = visited[player.current_room.id].pop(0)
+        #visited[player.current_room.id].pop()
+        #add that move to the traversal_path
+        traversal_path.append(move_to_next_room) 
+        #also add it to the main path
+        main_path.append(oppoiste_direction[move_to_next_room])
+        #go backwards through rooms
+        player.travel(move_to_next_room)
 
 
 
